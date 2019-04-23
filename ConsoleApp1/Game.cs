a6419205149ad5c3245c0f521d6548979282ad2d
Christophe Charlebois <christophe.charlebois@kiflo.com>
2019-04-23 10:43:20 +0200

    refactoring in process

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -101,9 +101,62 @@ namespace ConsoleApp1
             ParryActionFromDefenderPlayer(defenderPlayer, actionFromPlayer);
         }
 
-        public void PlayerFightScenario(PlayerActions actionFromPlayer1, PlayerActions actionFromPlayer2)
+        public void PlayerFightScenario(Gladiator player1, Gladiator player2)
         {
+            do
+            {
+                Console.WriteLine("Player 1, choose an action : 1 for Weak, 2 for Strong ou 3 for Parry");
+                PlayerActions actionFromPlayer1 = (PlayerActions)Convert.ToInt32(Console.ReadLine());
+                //   Console.WriteLine($"You choose {action}action");
+                Console.WriteLine("Player 2, choose an action : 1 for Weak, 2 for Strong ou 3 for Parry");
+                PlayerActions actionFromPlayer2 = (PlayerActions)Convert.ToInt32(Console.ReadLine());
 
+                if (actionFromPlayer1 == PlayerActions.Weak)
+                {
+                    WeakActionPlayerScenario(player1, player2, actionFromPlayer1);
+                }
+
+                if (actionFromPlayer2 == PlayerActions.Weak)
+                {
+                    WeakActionPlayerScenario(player2, player1, actionFromPlayer2);
+                }
+
+                if (actionFromPlayer1 == PlayerActions.Strong)
+                {
+                    StrongActionPlayerScenario(player1, player2, actionFromPlayer1);
+                }
+
+                if (actionFromPlayer2 == PlayerActions.Strong)
+                {
+                    StrongActionPlayerScenario(player2, player1, actionFromPlayer2);
+                }
+
+                if (actionFromPlayer1 == PlayerActions.Parry)
+                {
+                    ParryActionPlayerScenario(player1, actionFromPlayer1);
+                }
+
+                if (actionFromPlayer2 == PlayerActions.Parry)
+                {
+                    ParryActionPlayerScenario(player2, actionFromPlayer2);
+                }
+
+                Console.WriteLine($"Player 1's Life {player1.Pv}");
+                Console.WriteLine($"Player 2's Life {player2.Pv}");
+                Console.WriteLine($"Player 1's Stamina {player1.Stamina}");
+                Console.WriteLine($"Player 2's Stamina {player2.Stamina}");
+            } while (player1.Pv > 0 && player2.Pv > 0);
+
+            if (player2.Pv > player1.Pv)
+            {
+                Console.WriteLine($"Game is over mates !  Player 2 win");
+            }
+
+            if (player1.Pv > player2.Pv)
+            {
+                Console.WriteLine($"Game is over mates !  Player 1 win");
+            }
+            Console.ReadLine();
         }
 
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,60 +21,9 @@ namespace ConsoleApp1
 
             Console.WriteLine("Start fight !");
 
-            do
-            {
-                Console.WriteLine("Player 1, choose an action : 1 for Weak, 2 for Strong ou 3 for Parry");
-                PlayerActions actionFromPlayer1 = (PlayerActions)Convert.ToInt32(Console.ReadLine());
-                //   Console.WriteLine($"You choose {action}action");
-                Console.WriteLine("Player 2, choose an action : 1 for Weak, 2 for Strong ou 3 for Parry");
-                PlayerActions actionFromPlayer2 = (PlayerActions)Convert.ToInt32(Console.ReadLine());
+            game.PlayerFightScenario(player1, player2);
 
-                if (actionFromPlayer1 == PlayerActions.Weak)
-                {
-                    game.WeakActionPlayerScenario(player1, player2, actionFromPlayer1);
-                }
 
-                if (actionFromPlayer2 == PlayerActions.Weak)
-                {
-                    game.WeakActionPlayerScenario(player2, player1, actionFromPlayer2);
-                }
-
-                if (actionFromPlayer1 == PlayerActions.Strong)
-                {
-                    game.StrongActionPlayerScenario(player1, player2, actionFromPlayer1);
-                }
-
-                if (actionFromPlayer2 == PlayerActions.Strong)
-                {
-                    game.StrongActionPlayerScenario(player2, player1, actionFromPlayer2);
-                }
-
-                if (actionFromPlayer1 == PlayerActions.Parry)
-                {
-                    game.ParryActionPlayerScenario(player1, actionFromPlayer1);
-                }
-
-                if (actionFromPlayer2 == PlayerActions.Parry)
-                {
-                    game.ParryActionPlayerScenario(player2, actionFromPlayer2);
-                }
-
-              Console.WriteLine($"Player 1's Life {player1.Pv}");
-              Console.WriteLine($"Player 2's Life {player2.Pv}");
-              Console.WriteLine($"Player 1's Stamina {player1.Stamina}");
-              Console.WriteLine($"Player 2's Stamina {player2.Stamina}");
-            } while (player1.Pv > 0 && player2.Pv > 0);
-
-            if (player2.Pv > player1.Pv)
-            {
-                Console.WriteLine($"Game is over mates !  Player 2 win");
-            }
-
-            if (player1.Pv > player2.Pv)
-            {
-                Console.WriteLine($"Game is over mates !  Player 1 win");
-            }
-            Console.ReadLine();
         }
     }
 }
